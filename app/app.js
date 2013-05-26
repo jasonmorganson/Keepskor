d4766cbeed6728041b9b99f3a5915449f5d09568
Jason Morganson <jason@morganson.me>
2013-05-26 17:56:43 -0400

    Broadway doesnt support plugin dependencies yet, so move requires into main file and attach them to to app object

--- app/app.js
+++ app/app.js
@@ -2,6 +2,12 @@
 var flatiron = require('flatiron'),
     app = flatiron.app;
 
+app.flatiron = flatiron;
+app.winston = require('winston');
+app.Papertrail = require('winston-papertrail').Papertrail,
+app.Exceptional = require('exceptional-node').Exceptional;
+app.nodefly = require('nodefly');
+
 app.use(require('./config'));
 app.use(require('./logger'));
 
--- app/logger.js
+++ app/logger.js
@@ -1,51 +1,49 @@
 exports.attach = function (options) {
 
-    var app = this,
-        flatiron = require('flatiron'),
-        winston = require('winston'),
-        package = require('../package.json'),
-        Papertrail = require('winston-papertrail').Papertrail,
-        Exceptional = require('exceptional-node').Exceptional;
+var package = require('../package.json');
 
-    Exceptional.API_KEY = app.config.get("exceptional:api_key");
 
-    require('nodefly').profile(
+    var app = this;
+
+    app.Exceptional.API_KEY = app.config.get("exceptional:api_key");
+
+    app.nodefly.profile(
         app.config.get("nodefly:key"),
         package.name
     );
 
-    app.use(flatiron.plugins.log);
+    app.use(app.flatiron.plugins.log);
 
     var logger = app.log.get('default');
 
     logger.transports.console.colorize = true;
     logger.transports.console.prettyPrint = true;
 
-    logger.add( winston.transports.File, {
+    logger.add( app.winston.transports.File, {
         level: 'info',
         name: 'file.info',
         filename: 'log/info.log'
     });
 
-    logger.add( winston.transports.File, {
+    logger.add( app.winston.transports.File, {
         level: 'warn',
         name: 'file.warn',
         filename: 'log/warnings.log'
     });
 
-    logger.add( winston.transports.File, {
+    logger.add( app.winston.transports.File, {
         level: 'error',
         name: 'file.error',
         filename: 'log/errors.log'
     });
 
-    logger.add( winston.transports.File, {
+    logger.add( app.winston.transports.File, {
         handleExceptions: true,
         name: 'file.exceptions',
         filename: 'log/exceptions.log'
     });
 
-    logger.add( winston.transports.Papertrail,
+    logger.add( app.winston.transports.Papertrail,
         app.config.get('papertrail')
     );
 
@@ -58,6 +56,6 @@ exports.attach = function (options) {
     });
 
     process.addListener( 'uncaughtException', function(err) {
-        Exceptional.handle(err);
+        app.Exceptional.handle(err);
     });
 };
