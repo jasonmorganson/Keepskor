c232864143a58675c977cde45b45e4c335e19de9
Jason Morganson <jason@morganson.me>
2013-05-28 09:07:20 -0400

    Explictly include flatiron config plugin

--- app/config.js
+++ app/config.js
@@ -3,6 +3,8 @@ exports.attach = function() {
     var app = this,
         env = process.env.NODE_ENV || 'development';
 
+    app.use(app.flatiron.plugins.config);
+
     app.config
         .env()
         .argv()
