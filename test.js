bad00935273cfe31147afb73df3b90904210c53f
Jason Morganson <jason@morganson.me>
2013-06-03 08:45:59 -0400

    Move to send channel

--- test.js
+++ test.js
@@ -2,13 +2,48 @@ var request = require('request');
 var io = require('socket.io-client');
 var socket = io.connect('http://pong.keepskor.com:9090');
 
+var token = '';
 var playerKey = '';
 
-request.post('http://pong.keepskor.com/api/player', function(error, response, body) {
+var url = "http://pong.keepskor.com/api/player";
+
+var headers = {};
+    headers["Cookie"] = "ks_a";
+    headers["Origin"] = "";
+    headers["Access-Control-Allow-Origin"] = "*";
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+    headers["Access-Control-Allow-Credentials"] = true;
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authorization, Accept";
+
+var jar = request.jar();
+
+var cookie = request.cookie("test=t");
+
+jar.add(cookie);
+
+var options = {
+    url: url,
+    jar: jar,
+    headers: headers
+}
+
+
+request.post(options, function(error, response, body) {
     if(!error && response.statusCode == 200) {
+//        console.log(response);
+console.log("_disableCookies:" + response._disableCookies);
+        console.log(response.cookieJar);
+//          console.log(response.jar.get({ url: url }));
+//        console.log(response.headers);
+        console.log(response.headers['set-cookie']);
+        token = response.headers['x-request-id'];
         playerKey = body;
-        console.log("Player Key:");
-        console.log(playerKey)
+        console.log("Token: " + token);
+        console.log("Player Key: " + playerKey);
+        request.get('http://pong.keepskor.com/api/player', function(error, response, body) {
+//            console.log(response.headers);
+        });
     } else {
         console.log('error:' + response.statusCode);
         console.log(body);
@@ -16,14 +51,13 @@ request.post('http://pong.keepskor.com/api/player', function(error, response, bo
 });
 
 
-
-
-
 socket.on('connecting', function() {
     console.log("Connecting...");
 });
 
+
 socket.on('connect', function() {
+
     console.log("Connected");
 
     socket.on('server_response', function(data) {
@@ -32,13 +66,17 @@ socket.on('connect', function() {
 
     var verb = {
         'a': playerKey,
-        'b': 'register test pass',
-        'r': 'lobby'
+        'b': 'login',
+        'r': 'lobby',
+        's': token
     }
 
+    console.log(JSON.stringify(verb));
     console.log(verb);
 
-    socket.emit('server_response', verb, function(data) {
+    socket.emit('send', verb, function(data) {
         console.log(data);
     });
+
+    socket.emit('my other event', { my: verb });
 });
