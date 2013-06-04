bf692ae027a3d88d3dde6adc8685dc12afd6217a
Jason Morganson <jason@morganson.me>
2013-06-04 11:47:53 -0400

    More server tests

--- test/server.js
+++ test/server.js
@@ -8,11 +8,23 @@ var app = require('../app/app.js');
 
 describe('server', function(){
 
+    describe('should have', function() {
+        it('http router', function() {
+            expect(app.http.router).to.exist;
+        });
+    });
     describe('should be', function() {
+        it('using the http plugin', function() {
+            expect(app.http).to.exist;
+        });
+        it('using the resourceful plugin', function() {
+            expect(app.resources).to.exist;
+            expect(app.define).to.exist;
+        });
         it('listening', function() {
             expect(app.server.address()).to.exist;
             expect(app.server.address().address).to.not.be.undefined;
-            expect(app.server.address().port).to.not.be.undefined;
+            assert.isNumber(app.server.address().port);
         });
     });
 })
