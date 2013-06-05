7ca844707dcddf2d426bee9286bbe18c6cd1d523
Jason Morganson <jason@morganson.me>
2013-06-05 18:16:15 -0400

    Include more verbose logging

--- app/passport.js
+++ app/passport.js
@@ -1,34 +1,53 @@
-var passport = require('passport'),
+var errs = require('errs'),
+    passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
 
 exports.attach = function() {
 
     var app = this;
 
-    passport.serializeUser(function(user, done) {
+    app.passport = passport;
+    app.authStrategy = passport;
+
+    app.passport.serializeUser(function(user, done) {
+        app.log.debug("Serialize", user);
         done(null, user.username);
     });
 
-    passport.deserializeUser(function(username, done) {
-        app.resources.Player.findByUsername( username, function(error, user) {
-            done(error, user);
+    app.passport.deserializeUser(function(username, done) {
+        app.log.debug("Deserialize", username);
+        app.resources.User.find({ username: username }, function(err, user) {
+            done(err, user);
         });
     });
 
-    passport.use(new LocalStrategy( function(username, password, done) {
+    app.passport.use(new LocalStrategy(
 
-        app.resources.Player.findByUsername( username, function(error, user) {
+        function(username, password, done) {
 
-            if (error) {
-                return done(error);
-            }
-            if (!user) {
-                return done(null, false, { message: 'Unknown user ' + username });
-            }
-            if (user.password != password) {
-                return done(null, false, { message: 'Invalid password' });
-            }
-            return done(null, user);
-        })
-    }));
+            app.log.debug("Authenticate locally with " + username + " and " + password);
+
+            app.resources.User.find({ username: username }, function(err, user){
+
+                if (err) {
+                    app.log.error(errs.merge(err, "There was an error finding user"));
+                    return done(err);
+                }
+
+                console.log(user);
+                if (!user) {
+                    app.log.debug("Unknown user: " + username);
+                    return done(null, false, { message: "Unknown user " + username });
+                }
+
+                if (user.password != password) {
+                    app.log.warn("Invalid password for " + username);
+                    return done(null, false, { message: "Invalid password" });
+                }
+
+                app.log.debug("Authenticated");
+                return done(null, user);
+            });
+        }
+    ));
 };
--- app/routes.js
+++ app/routes.js
@@ -20,19 +20,30 @@ exports.attach = function() {
         });
 
     app.http.router.notfound = function(callback) {
-        app.log.warn("Not Found: " + this.req.url);
-        var headers = req.headers || { 'Content-Type': 'text/html' };
-        this.res.writeHead(404, headers);
-        this.res.end("Not Found");
-        callback(OK, this.req, this.res);
+
+        var req = this.req,
+            res = this.res,
+            headers = req.headers || { 'Content-Type': 'text/html' };
+            NotFound = new director.http.NotFound;
+
+        NotFound.message += ': ' + req.url;
+        res.writeHead(404, headers);
+        res.end(NotFound);
+        callback(NotFound, req, res);
     };
 
-    app.http.onError = function(err, req, res) {
+    var onError = app.http.onError = function(err, req, res) {
+
+        var status = err.status || 500,
+            headers = err.headers || { 'Content-Type': 'text/html' },
+            body = err.message || err.body.error || "Error";
+
         if(err) {
-            var status = err.status || 500;
-            var headers = err.headers || { 'Content-Type': 'text/html' };
-            var body = err.message || err.body.error || "Error";
-            app.log.error(body);
+
+            if (err.status >= 500) { app.log.error(body); }
+            else if (err.status >= 400) { app.log.warn(body); }
+            else { app.log.debug(body); }
+
             res.writeHead(status, headers);
             res.end(body);
         }
